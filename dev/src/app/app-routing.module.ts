a4a3a7e40097d3ec5372a1d5111aeab513a84f54
Yuri Shevchenko <jushevch@gmail.com>
2022-10-19 14:36:44 +0300

    Fix scroll position

--- dev/src/app/app-routing.module.ts
+++ dev/src/app/app-routing.module.ts
@@ -47,7 +47,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
